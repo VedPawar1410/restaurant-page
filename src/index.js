256c77b6b5173fd36f88325c237de26ccbb734f2
Ved Pawar <vedpawar1410@gmail.com>
2025-11-26 10:50:37 +0530

    tab switching best practice: using ids

--- src/index.js
+++ src/index.js
@@ -35,25 +35,45 @@ loadHomePage();
 //     }
 // });
 
-let btns = document.querySelectorAll('button');
+// let btns = document.querySelectorAll('button');
+// const content = document.getElementById("content");
+// btns.forEach((btn) => {
+//     btn.addEventListener('click', (e)=>{
+//         //debugging tip: console.log(`"${e.target.textContent}"`);
+//         if(e.target.textContent.trim() == "Home"){
+//             //const content = document.getElementById("content");
+//             content.innerHTML = "";
+//             loadHomePage();
+//         }
+//         else if(e.target.textContent.trim() == "Menu"){
+//             //const content = document.getElementById("content");
+//             content.innerHTML = "";
+//             menu();
+//         }
+//         else if(e.target.textContent.trim() == "Contact"){
+//             //const content = document.getElementById("content");
+//             content.innerHTML = "";
+//             contact();
+//         }
+//     });
+// })
+
 const content = document.getElementById("content");
-btns.forEach((btn) => {
-    btn.addEventListener('click', (e)=>{
-        //debugging tip: console.log(`"${e.target.textContent}"`);
-        if(e.target.textContent.trim() == "Home"){
-            //const content = document.getElementById("content");
-            content.innerHTML = "";
-            loadHomePage();
-        }
-        else if(e.target.textContent.trim() == "Menu"){
-            //const content = document.getElementById("content");
-            content.innerHTML = "";
-            menu();
-        }
-        else if(e.target.textContent.trim() == "Contact"){
-            //const content = document.getElementById("content");
-            content.innerHTML = "";
-            contact();
-        }
-    });
-})
+const homeBtn = document.getElementById("home-btn");
+const menuBtn = document.getElementById("menu-btn");
+const contactBtn = document.getElementById("contact-btn");
+
+homeBtn.addEventListener("click", () => {
+    content.innerHTML = "";
+    loadHomePage();
+});
+
+menuBtn.addEventListener("click", () => {
+    content.innerHTML = "";
+    menu();
+});
+
+contactBtn.addEventListener("click", () => {
+    content.innerHTML = "";
+    contact();
+});
