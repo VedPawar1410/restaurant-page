0cc0ee396d05e592d0a742888ec4336af99e8eb5
Ved Pawar <vedpawar1410@gmail.com>
2025-11-26 10:48:39 +0530

    tab switching logic added

--- src/index.js
+++ src/index.js
@@ -6,12 +6,54 @@ import { contact } from "./contact.js";
 //styles.css import
 
 
-// CLEAR content first (optional, but good practice)
-const content = document.getElementById("content");
-content.innerHTML = "";
+// // CLEAR content first (optional, but good practice)
+// const content = document.getElementById("content");
+// content.innerHTML = "";
 
 
 // Run the function to populate the page
 loadHomePage();
 // menu();
 // contact();
+
+// const btn = document.querySelector("button");
+// btn.addEventListener("click", (e)=>{
+//     if(e.target.textContent == "Home"){
+//         const content = document.getElementById("content");
+//         content.innerHTML = "";
+//         loadHomePage();
+//     }
+//     else if(e.target.textContent == "Menu"){
+//         const content = document.getElementById("content");
+//         content.innerHTML = "";
+//         menu();
+//     }
+//     else if(e.target.textContent == "Contact"){
+//         const content = document.getElementById("content");
+//         content.innerHTML = "";
+//         contact();
+//     }
+// });
+
+let btns = document.querySelectorAll('button');
+const content = document.getElementById("content");
+btns.forEach((btn) => {
+    btn.addEventListener('click', (e)=>{
+        //debugging tip: console.log(`"${e.target.textContent}"`);
+        if(e.target.textContent.trim() == "Home"){
+            //const content = document.getElementById("content");
+            content.innerHTML = "";
+            loadHomePage();
+        }
+        else if(e.target.textContent.trim() == "Menu"){
+            //const content = document.getElementById("content");
+            content.innerHTML = "";
+            menu();
+        }
+        else if(e.target.textContent.trim() == "Contact"){
+            //const content = document.getElementById("content");
+            content.innerHTML = "";
+            contact();
+        }
+    });
+})
